225ff36200996cf0c098f4b2509550803ec7b9a3
YumeMichi <do4suki@gmail.com>
2019-05-04 20:54:37 +0530

    onyx:- try enforcing for now , updated use of passthrough power and light hals , cleanup sepolicy

--- device.mk
+++ device.mk
@@ -197,7 +197,8 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.onyx
+    android.hardware.light@2.0-impl \
+    lights.msm8974
 
 # Limit dex2oat threads to improve thermals
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -235,7 +236,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service.onyx
+    android.hardware.power@1.0-impl \
+    power.msm8974
+
 
 # RIL
 PRODUCT_PACKAGES += \
