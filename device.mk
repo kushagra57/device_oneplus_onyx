0eeabdae403570ba73c6fdb9d9d96d2519b2f61d
Wang Han <416810799@qq.com>
2019-09-16 23:56:33 +0800

    onyx: Import and bind mount loader config for media swcodec
     * This config needs to be modified for legacy devices. As
       there is no easy way to split it, import it into device
       tree and bind mount it instead.
    
     * Head at android-10.0.0_r2
       (3ac371484399da6efbd0c835aa461d3326263ab1).
    
    Change-Id: I8d5003d7cd3a471c569844aede4b5ea4c68ae36b

--- device.mk
+++ device.mk
@@ -83,6 +83,10 @@ PRODUCT_COPY_FILES += \
 #    com.dsi.ant.antradio_library \
 #    libantradio
 
+# APEX
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/ld.config.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/swcodec/ld.config.txt
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/MTP_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/MTP/MTP_Bluetooth_cal.acdb \
